f80aea03801de954eab2ec79033414c3b8aaa8f7
brunoBMonteiro <brunobilheri@hotmail.com>
2021-10-09 14:16:29 -0300

    #011 programming exercise

--- ex011/src/InteirosEReal.java
+++ ex011/src/InteirosEReal.java
@@ -1,11 +1,23 @@
 import java.util.Scanner;
 
 public class InteirosEReal {
-    Scanner input = new Scanner(System.in);
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
 
-    int a = input.nextInt();
-    int b = input.nextInt();
-    double c = input.nextDouble();
+        System.out.println("Digite um numero para variavel A (inteiro)");
+        int a = input.nextInt();
+        System.out.println("Digite um numero para variavel B (inteiro)");
+        int b = input.nextInt();
+        System.out.println("Digite um numero para variavel C (double)");
+        double c = input.nextDouble();
+
+        int resultadoA = (a * 2) * (b / 2);
+        double resultadoB = (a * 3) +c;
+        double resultadoC = Math.pow(c, 3);
 
 
+        System.out.println("Valor de A :" + resultadoA);
+        System.out.println("Valor de B :" + resultadoB);
+        System.out.println("Valor de C :" + resultadoC);
+    }
 }
